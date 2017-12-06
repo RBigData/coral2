dff1559bf4c6a10922beeee6b8be407aa0591eda
wrathematics <wrathematics@gmail.com>
2017-12-06 10:33:00 -0500

    tweak svm benchmark

--- benchmarks/r/svm.r
+++ benchmarks/r/svm.r
@@ -1,12 +1,31 @@
 source("./utils.r")
 comm.set.seed(1234, diff=FALSE)
 
-x = ranshaq(rnorm, m.local, n, local=TRUE)
 
-data.y = matrix(ifelse(Data(x[, 1])>0, 1, -1), ncol=1)
-y = shaq(data.y, m.local, checks=FALSE)
+generator = function(m, n, means=c(0, 2))
+{
+  response = c(-1L, 1L)
+  
+  x = matrix(0.0, m, n+1)
+  y = integer(m)
+  
+  for (i in 1:m)
+  {
+    group = sample(2, size=1)
+    x[i, ] = c(1, rnorm(n, mean=means[group]))
+    y[i] = response[group]
+  }
+  
+  list(x=x, y=y)
+}
 
-time = comm.timer(svm(x, y))
+
+data = generator(m.local, n)
+m = allreduce(m.local)
+x = shaq(data$x, nrows=m, ncols=n+1)
+y = shaq(data$y, nrows=m, ncols=1)
+
+time = comm.timer(svm(x, y, maxiter=500))
 comm.print(time)
 
 finalize()
